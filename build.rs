e8aec9c4bde0542348ca32b2f1aeb4e63d3edaee
Alex Vlasov <alex.m.vlasov@gmail.com>
2019-07-29 13:34:53 +0300

    introduce feature for fuzzing

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
 extern crate cc;
 
 fn main() {
+    #[cfg(not(feature = "fuzz"))]
     cc::Build::new()
         .cpp(true) // Switch to C++ library compilation.
         .flag("-std=c++1z")
+        .flag("-static-libstdc++")
         .include("eip1962cpp/include")
         .file("eip1962cpp/src/api.cpp")
         .file("eip1962cpp/src/common.cpp")
@@ -13,4 +15,38 @@ fn main() {
         .static_flag(true)
         .opt_level_str("3")
         .compile("eip1962cpp.a");
+
+    #[cfg(all(feature = "fuzz", not(target_os = "macos")))]
+    cc::Build::new()
+        .cpp(true) 
+        .flag("-std=c++1z")
+        .flag("-static-libstdc++")
+        .flag("-fuse-ld=lld")
+        .flag("-fsanitize=fuzzer,undefined,address") // Additional flags for fuzzing
+        .include("eip1962cpp/include")
+        .file("eip1962cpp/src/api.cpp")
+        .file("eip1962cpp/src/common.cpp")
+        .file("eip1962cpp/src/wrapper.cpp")
+        .file("eip1962cpp/src/repr.cpp")
+        .warnings(false)
+        .static_flag(true)
+        // .opt_level_str("3")
+        .compile("eip1962cpp_fuzz.a");
+
+    #[cfg(all(feature = "fuzz", target_os = "macos"))]
+    cc::Build::new()
+        .cpp(true) 
+        .flag("-std=c++1z")
+        .flag("-static-libstdc++")
+        .flag("-fsanitize=fuzzer,undefined") // Additional flags for fuzzing
+        .flag("-fuse-ld=lld")
+        .include("eip1962cpp/include")
+        .file("eip1962cpp/src/api.cpp")
+        .file("eip1962cpp/src/common.cpp")
+        .file("eip1962cpp/src/wrapper.cpp")
+        .file("eip1962cpp/src/repr.cpp")
+        .warnings(false)
+        .static_flag(true)
+        // .opt_level_str("3")
+        .compile("eip1962cpp_fuzz_macos.a");
 }
