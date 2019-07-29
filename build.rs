927169c40c3a049a09a7eefa74ea264ecc751e1a
Alex Vlasov <alex.m.vlasov@gmail.com>
2019-07-29 14:31:45 +0300

    check that fuzzing builds are with clang

--- build.rs
+++ build.rs
@@ -17,24 +17,31 @@ fn main() {
         .compile("eip1962cpp.a");
 
     #[cfg(all(feature = "fuzz", not(target_os = "macos")))]
-    cc::Build::new()
-        .cpp(true) 
-        .flag("-std=c++1z")
-        // .flag("-static-libstdc++")
-        // .flag("-fuse-ld=lld")
-        .flag("-fsanitize=fuzzer,undefined,address") // Additional flags for fuzzing
-        .include("eip1962cpp/include")
-        .file("eip1962cpp/src/api.cpp")
-        .file("eip1962cpp/src/common.cpp")
-        .file("eip1962cpp/src/wrapper.cpp")
-        .file("eip1962cpp/src/repr.cpp")
-        .warnings(false)
-        // .static_flag(true)
-        // .opt_level_str("3")
-        .compile("eip1962cpp_fuzz.a");
+    {
+        let params = cc::Build::new()
+            .cpp(true) 
+            .flag("-std=c++1z")
+            // .flag("-static-libstdc++")
+            // .flag("-fuse-ld=lld")
+            .flag("-fsanitize=fuzzer,undefined,address") // Additional flags for fuzzing
+            .include("eip1962cpp/include")
+            .file("eip1962cpp/src/api.cpp")
+            .file("eip1962cpp/src/common.cpp")
+            .file("eip1962cpp/src/wrapper.cpp")
+            .file("eip1962cpp/src/repr.cpp")
+            .warnings(false);
+            // .static_flag(true)
+            // .opt_level_str("3")
+        let instrument = params.get_compiler();
+        if !instrument.is_like_clang() {
+            panic("Fuzzing target should be compiled by Clange");
+        }
+        params.compile("eip1962cpp_fuzz.a");
+    }
 
     #[cfg(all(feature = "fuzz", target_os = "macos"))]
-    cc::Build::new()
+    {
+        let params = cc::Build::new()
         .cpp(true) 
         .flag("-std=c++1z")
         // .flag("-static-libstdc++")
@@ -45,8 +52,13 @@ fn main() {
         .file("eip1962cpp/src/common.cpp")
         .file("eip1962cpp/src/wrapper.cpp")
         .file("eip1962cpp/src/repr.cpp")
-        .warnings(false)
+        .warnings(false);
         // .static_flag(true)
         // .opt_level_str("3")
-        .compile("eip1962cpp_fuzz_macos.a");
+        let instrument = params.get_compiler();
+        if !instrument.is_like_clang() {
+            panic("Fuzzing target should be compiled by Clange");
+        }
+        params.compile("eip1962cpp_fuzz.a");
+    }
 }
