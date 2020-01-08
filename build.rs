5b10df19558f628b64e369c713810b3c68c62872
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-08 20:00:28 +0200

    return optimization mode

--- build.rs
+++ build.rs
@@ -6,7 +6,7 @@ fn main() {
         cc::Build::new()
             .cpp(true) // Switch to C++ library compilation.
             .flag("-std=c++1z")
-            // .flag("-static-libstdc++")
+            .flag("-static-libstdc++")
             .include("eip1962cpp/include")
             .file("eip1962cpp/src/api.cpp")
             .file("eip1962cpp/src/common.cpp")
@@ -15,7 +15,7 @@ fn main() {
             .file("eip1962cpp/src/gas_meter.cpp")
             .warnings(false)
             .static_flag(true)
-            // .opt_level_str("3")
+            .opt_level_str("3")
             .compile("eip1962cpp.a");
     }
 
@@ -25,7 +25,7 @@ fn main() {
             .cpp(true) // Switch to C++ library compilation.
             .flag("-std=c++1z")
             .flag("-DFUZZING")
-            // .flag("-static-libstdc++")
+            .flag("-static-libstdc++")
             .include("eip1962cpp/include")
             .file("eip1962cpp/src/api.cpp")
             .file("eip1962cpp/src/common.cpp")
@@ -34,7 +34,7 @@ fn main() {
             .file("eip1962cpp/src/gas_meter.cpp")
             .warnings(false)
             .static_flag(true)
-            // .opt_level_str("3")
+            .opt_level_str("3")
             .compile("eip1962cpp.a");
     }
 
